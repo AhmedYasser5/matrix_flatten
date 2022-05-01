ef88687bfa82e418c85b86c83552306b8a5b9fdf
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-05-01 22:05:54 +0200

    testing the functions

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "matrix.h"
+#include <iostream>
+using namespace std;
+int main() {
+  int n, m, p;
+  cin >> n >> m >> p;
+  matrix<int> mat(n, m, p);
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      for (int k = 0; k < p; k++)
+        mat[mat.convertIndex(i, j, k)] = i + j + k;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      for (int k = 0; k < p; k++)
+        cout << mat[mat.convertIndex(i, j, k)] << ' ';
+  return 0;
+}
